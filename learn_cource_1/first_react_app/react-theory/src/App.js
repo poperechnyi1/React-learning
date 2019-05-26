783abe0dc150640c7601a0ec735f71e11cc3f2e4
spoperechnyi <poperechnyi1@gmail.com>
2019-05-26 17:36:08 +0300

    html instead clear js

--- learn_cource_1/first_react_app/react-theory/src/App.js
+++ learn_cource_1/first_react_app/react-theory/src/App.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hello World!</h1>
-  //   </div>
-  // );
-  return React.createElement(
-    'div',
-    {
-      className: 'App'
-    },
-    React.createElement(
-      'h1',
-      null,
-      'Hello world!'
-    )
-    
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
   );
+  // return React.createElement(
+  //   'div',
+  //   {
+  //     className: 'App'
+  //   },
+  //   React.createElement(
+  //     'h1',
+  //     null,
+  //     'Hello world!'
+  //   )
+    
+  // );
 }
 
 export default App;
