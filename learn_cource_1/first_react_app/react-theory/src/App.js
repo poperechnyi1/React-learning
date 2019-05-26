e940469913f3b19aa0cf5b6319e9186ee2e65f96
spoperechnyi <poperechnyi1@gmail.com>
2019-05-26 17:43:47 +0300

    always we need main div

--- learn_cource_1/first_react_app/react-theory/src/App.js
+++ learn_cource_1/first_react_app/react-theory/src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div>
+          <p>Hello</p>
     </div>
+   
   );
-  // return React.createElement(
-  //   'div',
-  //   {
-  //     className: 'App'
-  //   },
-  //   React.createElement(
-  //     'h1',
-  //     null,
-  //     'Hello world!'
-  //   )
-    
-  // );
+ 
 }
 
 export default App;
