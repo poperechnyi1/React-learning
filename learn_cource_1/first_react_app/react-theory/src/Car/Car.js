4fe26f3867186d314ee7c42bd96a58cd7e1e8e12
spoperechnyi <poperechnyi1@gmail.com>
2019-05-26 21:53:42 +0300

    variants of creation component

--- learn_cource_1/first_react_app/react-theory/src/Car/Car.js
+++ learn_cource_1/first_react_app/react-theory/src/Car/Car.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-function Car(){
-    return (
-        <h2>This is a car component</h2>
-    )
-}
+// function Car(){
+//     return (
+//         <h2>This is a car component</h2>
+//     )
+// }
 
-export default Car;
+// const car = () => {
+//     return (
+//         <div>This is a car component</div>
+//     )
+// }
+
+// const car = () => <div>This is a car <strong>component</strong></div>
+
+export default () => (<div>This is a car <strong>component</strong></div>);
