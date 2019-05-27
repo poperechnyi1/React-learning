972ca8da7b6f73d90c71e52dcc66470bd261d9b4
spoperechnyi <poperechnyi1@gmail.com>
2019-05-27 22:11:00 +0300

    pass a content in component

--- learn_cource_1/first_react_app/react-theory/src/App.js
+++ learn_cource_1/first_react_app/react-theory/src/App.js
@@ -10,8 +10,12 @@ function App() {
   return (
       <div style={divStyle}>
         <h1>Hello World!</h1>
-        <Car name={'Ford'} year={2018}/>
-        <Car name={'Audi'} year={2016}/>
+        <Car name={'Ford'} year={2018}>
+        <p style={{color:'blue'}}>Color</p>
+        </Car>
+        <Car name={'Audi'} year={2016}>
+          <p style={{color:'red'}}>Color</p>
+        </Car>
         <Car name={'Mazda'} year={2010}/>
       </div>   
   );
--- learn_cource_1/first_react_app/react-theory/src/Car/Car.js
+++ learn_cource_1/first_react_app/react-theory/src/Car/Car.js
@@ -4,5 +4,6 @@ export default props => (
 <div>
     <h3>Can name: {props.name}</h3>
     <p>Number: <strong>{props.year}</strong></p>
+    { props.children }
 </div>
 );
