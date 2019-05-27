0b64712396fce8e9b4b04cb5d27bec6d3bf6ab52
spoperechnyi <poperechnyi1@gmail.com>
2019-05-27 21:43:35 +0300

    learn simple dinamic datas

--- learn_cource_1/first_react_app/react-theory/src/Car/Car.js
+++ learn_cource_1/first_react_app/react-theory/src/Car/Car.js
@@ -1,17 +1,8 @@
 import React from 'react';
 
-// function Car(){
-//     return (
-//         <h2>This is a car component</h2>
-//     )
-// }
-
-// const car = () => {
-//     return (
-//         <div>This is a car component</div>
-//     )
-// }
-
-// const car = () => <div>This is a car <strong>component</strong></div>
-
-export default () => (<div>This is a car <strong>component</strong></div>);
+export default () => (
+<div>
+    <p>This is a car</p>
+    <p>Number: <strong>{Math.round(Math.random()* 100)}</strong></p>
+</div>
+);
