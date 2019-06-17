eec642f4b2b608b103ed1e4c5165bb33f748a9a5
spoperechnyi <poperechnyi1@gmail.com>
2019-06-17 21:53:31 +0300

    handle input

--- learn_cource_1/first_react_app/react-theory/src/App.js
+++ learn_cource_1/first_react_app/react-theory/src/App.js
@@ -23,6 +23,10 @@ class App extends React.Component {
       pageTitle: newTitle
     })
   }
+
+  handleInput = (event)=>{
+    this.setState({pageTitle: event.target.value})
+  }
   
   render () {
     console.log('Render')
@@ -34,6 +38,7 @@ class App extends React.Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+        <input type='text' onChange={this.handleInput}/>
         <button 
         onClick={this.changeTitleHandler.bind(this, 'Changed!')}
         >Change title</button>
