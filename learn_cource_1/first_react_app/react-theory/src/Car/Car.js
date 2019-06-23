d5ccaecbf663a6eb3d7f6fe31b21ab679d6d6b3c
spoperechnyi <poperechnyi1@gmail.com>
2019-06-23 19:01:28 +0300

    work with css as with object

--- learn_cource_1/first_react_app/react-theory/src/Car/Car.js
+++ learn_cource_1/first_react_app/react-theory/src/Car/Car.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import Radium from 'radium';
-import './Car.css'
+import classes from './Car.css';
 
 const Car = props => {
 
-    const inputClasses = ['input'];
+    const inputClasses = [classes.input];
 
     if(props.name !== ''){
-        inputClasses.push('green');
+        inputClasses.push(classes.green);
     } else {
-        inputClasses.push('red');
+        inputClasses.push(classes.red);
     }
 
     if(props.name.length > 4) {
-        inputClasses.push('bold')
+        inputClasses.push(classes.bold)
     }
 
     const style = {
@@ -26,7 +26,7 @@ const Car = props => {
     }
 
 return (
-<div className = 'Car' style={style}>
+<div className = {classes.Car} style={style}>
     <h3>Can name: {props.name}</h3>
     <p>Number: <strong>{props.year}</strong></p>
     <input 
