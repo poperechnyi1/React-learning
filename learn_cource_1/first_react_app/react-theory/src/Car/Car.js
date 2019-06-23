6cf8219624f2da852bb4915df7ab471bb239d84b
spoperechnyi <poperechnyi1@gmail.com>
2019-06-23 17:55:50 +0300

    add conditions for this styles

--- learn_cource_1/first_react_app/react-theory/src/Car/Car.js
+++ learn_cource_1/first_react_app/react-theory/src/Car/Car.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './Car.css'
 
-export default props => (
+export default props => {
+
+    const inputClasses = ['input'];
+
+    if(props.name !== ''){
+        inputClasses.push('green');
+    } else {
+        inputClasses.push('red');
+    }
+
+    if(props.name.length > 4) {
+        inputClasses.push('bold')
+    }
+
+return (
 <div className = 'Car'>
     <h3>Can name: {props.name}</h3>
     <p>Number: <strong>{props.year}</strong></p>
-    <input type="text" onChange={props.onchangeName} value={props.name}/>
+    <input 
+        type="text" 
+        onChange={props.onchangeName} 
+        value={props.name}
+        className={inputClasses.join(' ')}
+    />
     <button onClick={props.onDelete}>Delete</button>
 </div>
-);
+)
+};
