3121878d7339cbd8ad8b746484f41625cecbb6ce
spoperechnyi <poperechnyi1@gmail.com>
2019-06-30 17:15:25 +0300

    write counter

---
+++ learn_cource_1/first_react_app/react-theory/src/Counter/Counter.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import Auxiliary from '../hoc/Auxialiary';
+
+export default class Counter extends Component {
+    state = {
+        counter: 0
+    }
+
+    addCounter =() =>{
+        this.setState({
+            counter: this.state.counter + 1
+        })
+    }
+
+
+    render () {
+        return (
+            <Auxiliary>
+                <h2>Counter {this.state.counter}</h2>
+                <button onClick={this.addCounter}>+</button>
+                <button onClick={()=>this.setState({counter: this.state.counter -1})}>-</button>
+            </Auxiliary>
+        )
+
+        // return  [
+        //         <h2 key={'1'}>Counter {this.state.counter}</h2>,
+        //         <button key={'2'} onClick={this.addCounter}>+</button>,
+        //         <button key={'3'} onClick={()=>this.setState({counter: this.state.counter -1})}>-</button>
+        //     ]
+    }
+}
