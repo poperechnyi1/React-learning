faef7807dcba0ab48b5173009030eff289eb5716
spoperechnyi <poperechnyi1@gmail.com>
2019-07-15 22:44:27 +0300

    Answer list is added

---
+++ learn_cource_1/component_practice/src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from './AnswersList.css';
+import AnswerItem from './AnswerItem/AnswerItem';
+
+const AnswersList = props =>(
+    <ul className={classes.AnswersList}>
+        {props.answers.map((answer, index)=>{
+            console.log(answer)
+            
+            return (
+                <AnswerItem answer={answer} key={index}/>
+            )
+        })}
+    </ul>
+)
+
+export default AnswersList;
