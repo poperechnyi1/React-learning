03244ee4e98b0403836adfad18cd8f2d6a611c3f
spoperechnyi <poperechnyi1@gmail.com>
2019-09-01 13:08:30 +0300

    Input was imported

--- learn_cource_1/forms/practice/src/containers/Auth/Auth.js
+++ learn_cource_1/forms/practice/src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import classes from './Auth.css'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
 
@@ -22,8 +23,13 @@ export default class Auth extends Component {
         <div>
           <h1>Авторизация</h1>
           <form onSubmit={this.submitHandler} className={classes.AuthForm}>
-            <input type='text'/>
-            <input type='text'/>
+            
+            <Input 
+              label='Email'
+            />
+            <Input 
+              label='Пароль'
+            />
 
             <Button 
               type="success" 
