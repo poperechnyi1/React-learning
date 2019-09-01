b09312be1e17f7343615b44e6d3e9a3fbc1d7bc9
spoperechnyi <poperechnyi1@gmail.com>
2019-09-01 15:47:02 +0300

    Make disabled forms

--- learn_cource_1/forms/practice/src/containers/Auth/Auth.js
+++ learn_cource_1/forms/practice/src/containers/Auth/Auth.js
@@ -7,6 +7,7 @@ import is from 'is_js'
 export default class Auth extends Component {
 
   state = {
+    isFormValid: false,
     formControls: {
       email: {
         value: '',
@@ -81,8 +82,15 @@ export default class Auth extends Component {
     control.valid = this.validateControl(control.value, control.validation);
     formControls[controlName] = control;
 
+
+    let isFormValid = true;
+    Object.keys(formControls).forEach(name => {
+      isFormValid = formControls[name].valid && isFormValid
+    })
+
     this.setState({
-      formControls
+      formControls,
+      isFormValid
     })
 
   }
@@ -106,6 +114,7 @@ export default class Auth extends Component {
   }
 
   render() {
+
     return (
       <div className={classes.Auth}>
         <div>
@@ -117,6 +126,7 @@ export default class Auth extends Component {
             <Button 
               type="success" 
               onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
             >
               Войти
             </Button>
@@ -124,6 +134,7 @@ export default class Auth extends Component {
             <Button 
               type="primary" 
               onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
             >
               Зарегистрироваться
             </Button>
