8e302e4ec46e4c34993aca192200c80cf5ad47ee
spoperechnyi <poperechnyi1@gmail.com>
2019-09-08 20:24:06 +0300

    Work with middleware

--- learn_cource_1/redux/theory/src/index.js
+++ learn_cource_1/redux/theory/src/index.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import Redux, {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import rootReducer from './redux/rootReducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./redux/rootReducer";
 
-const store = createStore(rootReducer);
+const loggerMiddleware = store => next => action => {
+  const result = next(action);
+  console.log("Middleware", store.getState());
+  return result;
+};
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 const app = (
-    <Provider store={store}>
-        <App/>
-    </Provider>
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
