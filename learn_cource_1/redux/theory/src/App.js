3f26002aa47237ffe35aa26fa2c735a771b9be8a
spoperechnyi <poperechnyi1@gmail.com>
2019-09-08 21:17:45 +0300

    code formated

--- learn_cource_1/redux/theory/src/App.js
+++ learn_cource_1/redux/theory/src/App.js
@@ -1,65 +1,104 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import './App.scss'
-import Counter from './Counter'
-import {add, sub, addNumberFromInput, subNumberFromInput} from './redux/actions/actions'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./App.scss";
+import Counter from "./Counter";
+import {
+  add,
+  sub,
+  addNumberFromInput,
+  subNumberFromInput,
+  asyncAdd
+} from "./redux/actions/actions";
 
 class App extends Component {
   state = {
     counter: 0,
     numberInInput: 0
-  }
+  };
 
-  updateCounter(value) {
-    
-  }
+  updateCounter(value) {}
 
   render() {
     return (
-      <div className={'App'}>
-        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
+      <div className={"App"}>
+        <h1>
+          Счетчик <strong>{this.props.counter}</strong>
+        </h1>
 
-        <hr/>
+        <hr />
 
         <div className="Actions">
           <button onClick={this.props.onAdd}>Добавить 1</button>
           <button onClick={this.props.onSub}>Вычесть 1</button>
-          <button onClick={() => this.props.onAddFromInput(Number(this.state.numberInInput))}>Добавить число из инпута</button>
+          <button
+            onClick={() =>
+              this.props.onAddFromInput(Number(this.state.numberInInput))
+            }
+          >
+            Добавить число из инпута
+          </button>
         </div>
 
-        <hr/>
+        <hr />
 
-        <input onChange={(event) => {
-          this.setState({
-            numberInInput: event.target.value
-          })
-        }}/>
-        <br/>
+        <input
+          onChange={event => {
+            this.setState({
+              numberInInput: event.target.value
+            });
+          }}
+        />
+        <br />
         <div className="Actions">
-          <button onClick={() => this.props.onAddFromInput(Number(this.state.numberInInput))}>Добавить число из инпута</button>
-          <button onClick={() => this.props.onSubFromInput(Number(this.state.numberInInput))}>Вычесть число из инпута</button>
-         
+          <button
+            onClick={() =>
+              this.props.onAddFromInput(Number(this.state.numberInInput))
+            }
+          >
+            Добавить число из инпута
+          </button>
+          <button
+            onClick={() =>
+              this.props.onSubFromInput(Number(this.state.numberInInput))
+            }
+          >
+            Вычесть число из инпута
+          </button>
         </div>
-        
+
+        <div>
+          <button
+            onClick={() =>
+              this.props.onAsyncAdd(100)
+            }
+          >
+            Асинхронно добавить 100
+          </button>
+        </div>
+
         <Counter />
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    counter: state.counter1.counter,
-  }
+    counter: state.counter1.counter
+  };
 }
 
-function mapDispatchToProps(dispatch){
+function mapDispatchToProps(dispatch) {
   return {
     onAdd: () => dispatch(add()),
     onSub: () => dispatch(sub()),
-    onAddFromInput: (number) => dispatch(addNumberFromInput(number)),
-    onSubFromInput: (number) => dispatch(subNumberFromInput(number)),
-  }
+    onAddFromInput: number => dispatch(addNumberFromInput(number)),
+    onSubFromInput: number => dispatch(subNumberFromInput(number)),
+    onAsyncAdd: number => dispatch(asyncAdd(number))
+  };
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
