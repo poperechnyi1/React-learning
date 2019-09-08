46214de2cea7ed97e88b1e9bbc2b3f7b6424e77b
spoperechnyi <poperechnyi1@gmail.com>
2019-09-08 21:35:02 +0300

    React devtools

--- learn_cource_1/redux/theory/src/index.js
+++ learn_cource_1/redux/theory/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import reduxThunk from "redux-thunk";
 import rootReducer from "./redux/rootReducer";
+
+
+const composeEnhancers = typeof window === 'object' && typeof window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ !== 'undefined' ?
+ window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+  
+}) : compose;
+
 
 const loggerMiddleware = store => next => action => {
   const result = next(action);
@@ -14,7 +21,7 @@ const loggerMiddleware = store => next => action => {
   return result;
 };
 
-const store = createStore(rootReducer, applyMiddleware(loggerMiddleware, reduxThunk));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(loggerMiddleware, reduxThunk)));
 
 const app = (
   <Provider store={store}>
