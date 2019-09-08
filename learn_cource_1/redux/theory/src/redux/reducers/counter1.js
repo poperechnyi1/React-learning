e7c577edc38a0f7f336212104e20f8b0e5c487ca
spoperechnyi <poperechnyi1@gmail.com>
2019-09-08 21:00:38 +0300

    Use new entity

--- learn_cource_1/redux/theory/src/redux/reducers/counter1.js
+++ learn_cource_1/redux/theory/src/redux/reducers/counter1.js
@@ -1,22 +1,24 @@
+import {ADD, SUB, ADD_INPUT, SUB_INPUT} from '../actions/actionTypes'
+
 const intialState = {
     counter: 0
   };
   
   export default function counter1(state = intialState, action) {
     switch (action.type) {
-      case "ADD":
+      case ADD:
         return {
           counter: state.counter + 1
         };
-      case "SUB":
+      case SUB:
         return {
           counter: state.counter - 1
         };
-      case "ADD_INPUT":
+      case ADD_INPUT:
         return {
           counter: state.counter + action.payload
         };
-      case "SUB_INPUT":
+      case SUB_INPUT:
         return {
           counter: state.counter - action.payload
         };
